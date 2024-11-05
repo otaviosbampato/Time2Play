207a5c6a9a06edb07739a00a95723118225abd12
Otávio Sbampato <otaviosbampato@gmail.com>
2024-11-05 17:56:14 -0300

    :sparkles: feat: pastas e criação SignUp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import "./App.css"
 
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-import Welcome from "./pages/Unsigned/Welcome";
+import Welcome from "./pages/Unsigned/Welcome/Welcome";
 
 function App() {
 
