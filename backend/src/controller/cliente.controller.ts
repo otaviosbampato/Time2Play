98c4081e1f8889bbf8176e1f251f6cd0ce368a7b
Frederico Maia Estrella <91262593+FredMaia@users.noreply.github.com>
2024-12-14 12:49:37 -0300

    :recycle: refactor: instanciar prisma em um único arquivo

--- backend/src/controller/cliente.controller.ts
+++ backend/src/controller/cliente.controller.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from "express";
 import bcryptjs from "bcryptjs";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "../db/prisma"
 
 export const registrar = async (req: Request, res: Response) => {
   try {
