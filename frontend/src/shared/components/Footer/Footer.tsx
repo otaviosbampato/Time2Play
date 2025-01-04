e39781ed855a2ff4c15144316f433f3d7f311488
Otávio Sbampato <otaviosbampato@gmail.com>
2025-01-03 23:47:50 -0300

    :sparkles: feat: adicionar componente footer

---
+++ frontend/src/shared/components/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <div>
+                <img src="src/assets/logofooter.png" className={styles.logo} />
+            </div>
+            <div>
+                <img src="src/assets/copyright.png" className={styles.copyright} />
+            </div>
+            <p className={styles.dataEDireitos}>2025 - Todos os direitos reservados.</p>
+            <div className={styles.divLinks}>
+                
+                <a href="https://www.figma.com/design/PimWxqrNvyG3GYz03faFwm/Time2Play?node-id=266-537&t=rYb5RVJY30BPEYXU-0" className={styles.politicaPrivacidade} target="_blank">Política de privacidade</a>
+                <a href="https://www.figma.com/design/PimWxqrNvyG3GYz03faFwm/Time2Play?node-id=266-537&t=rYb5RVJY30BPEYXU-0" className={styles.cookies} target="_blank">Cookies</a>
+                <a href="https://www.figma.com/design/PimWxqrNvyG3GYz03faFwm/Time2Play?node-id=266-537&t=rYb5RVJY30BPEYXU-0" className={styles.cookies} target="_blank">Termos de uso</a>
+            </div>
+        </footer>
+    )
+}
