9096cd8dc7de11132b1463adac750f728a9303cf
Frederico Maia Estrella <91262593+FredMaia@users.noreply.github.com>
2025-01-16 19:57:44 -0300

    :rocket: feat: add crud quadra

--- backend/src/controller/quadra.ts
+++ backend/src/controller/quadra.ts
@@ -3,7 +3,9 @@ import prisma from "../db/prisma";
 
 export const cadastrarQuadra = async (req: Request, res: Response) => {
   try {
-    const { nomeQuadra, precoHora, cidade, estado, endereco, proprietarioId } = req.body;
+    const { nomeQuadra, precoHora, cidade, estado, endereco } = req.body;
+
+    const proprietarioId = req.id;
 
     const proprietario = await prisma.proprietario.findUnique({
       where: { idProprietario: proprietarioId },
@@ -34,5 +36,106 @@ export const cadastrarQuadra = async (req: Request, res: Response) => {
     });
   } catch (error) {
     console.error("Erro ao cadastrar a quadra:", error);
-  } 
+    return res.status(500).json({ error: "Erro ao cadastrar a quadra." });
+  }
+};
+
+export const excluirQuadra = async (req: Request, res: Response) => {
+  try {
+    const { idQuadra } = req.params;
+    const proprietarioId = req.id;
+
+    const quadra = await prisma.quadra.findUnique({
+      where: { idQuadra: parseInt(idQuadra) },
+    });
+
+    if (!quadra) {
+      return res.status(404).json({ error: "Quadra não encontrada." });
+    }
+
+    if (quadra.proprietarioId !== proprietarioId) {
+      return res.status(403).json({ error: "Você não tem permissão para excluir essa quadra." });
+    }
+
+    const quadraExcluida = await prisma.quadra.delete({
+      where: { idQuadra: parseInt(idQuadra) },
+    });
+
+    console.log("Quadra excluída com sucesso:", quadraExcluida);
+
+    return res.status(200).send({
+      message: "Quadra excluída com sucesso.",
+      quadraExcluida: quadraExcluida,
+    });
+
+  } catch (error) {
+    console.error("Erro ao excluir a quadra:", error);
+    return res.status(500).json({ error: "Erro ao excluir a quadra." });
+  }
+};
+
+export const editarQuadra = async (req: Request, res: Response) => {
+  try {
+    const { idQuadra } = req.params;
+    const { nomeQuadra, precoHora, cidade, estado, endereco } = req.body;
+    
+    const proprietarioId = req.id;
+
+    const quadra = await prisma.quadra.findUnique({
+      where: { idQuadra: parseInt(idQuadra) },
+    });
+
+    if (!quadra) {
+      return res.status(404).json({ error: "Quadra não encontrada." });
+    }
+
+    if (quadra.proprietarioId !== proprietarioId) {
+      return res.status(403).json({ error: "Você não tem permissão para editar essa quadra." });
+    }
+
+    const updatedQuadra = await prisma.quadra.update({
+      where: { idQuadra: parseInt(idQuadra) },
+      data: {
+        nomeQuadra: nomeQuadra || quadra.nomeQuadra,
+        precoHora: precoHora || quadra.precoHora,
+        cidade: cidade || quadra.cidade,
+        estado: estado || quadra.estado,
+        endereco: endereco || quadra.endereco,
+      },
+    });
+
+    console.log("Quadra atualizada com sucesso:", updatedQuadra);
+
+    return res.status(200).send({
+      message: "Quadra atualizada com sucesso.",
+      updatedQuadra: updatedQuadra,
+    });
+  } catch (error) {
+    console.error("Erro ao editar a quadra:", error);
+    return res.status(500).json({ error: "Erro ao editar a quadra." });
+  }
+};
+
+export const verQuadra = async (req: Request, res: Response) => {
+  try {
+    const { idQuadra } = req.params;
+
+    const quadra = await prisma.quadra.findUnique({
+      where: { idQuadra: parseInt(idQuadra) },
+    });
+
+    if (!quadra) {
+      return res.status(404).json({ error: "Quadra não encontrada." });
+    }
+
+    console.log("Detalhes da quadra:", quadra);
+
+    return res.status(200).send({
+      message: "Detalhes da quadra encontrados.",
+      quadra: quadra,
+    });
+  } catch (error) {
+    console.error("Erro ao buscar a quadra:", error);
+    return res.status(500).json({ error: "Erro ao buscar a quadra." });
+  }
 };
--- backend/src/routes/quadra.ts
+++ backend/src/routes/quadra.ts
@@ -1,8 +1,13 @@
 import express from "express";
-import { cadastrarQuadra } from "../controller/quadra.ts";
+import { cadastrarQuadra, editarQuadra, excluirQuadra, verQuadra } from "../controller/quadra.ts";
+import admMiddleware from "../middleware/admMiddleware.ts";
+import authMiddleware from "../middleware/authMiddleware.ts"
 
 const router = express.Router();
 
-router.post("/cadastrar", cadastrarQuadra);
+router.post("/cadastrar", [authMiddleware, admMiddleware], cadastrarQuadra);
+router.delete("/excluir/:idQuadra", [authMiddleware, admMiddleware], excluirQuadra);
+router.put("/editar/:idQuadra", [authMiddleware, admMiddleware], editarQuadra);
+router.get("/verQuadra/:idQuadra", authMiddleware, verQuadra);
 
 export default router;
