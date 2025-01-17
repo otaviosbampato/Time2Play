0e00ab8d2b35e23534cb75d31443e9814949b3e1
Frederico Maia Estrella <91262593+FredMaia@users.noreply.github.com>
2025-01-17 15:42:54 -0300

    :rocket: feat: add tratamento de imagens com cloudinary

---
+++ backend/src/services/cloudinary.ts
@@ -0,0 +1,37 @@
+import nodemailer, { Transporter } from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+interface VerifyUserEmailParams {
+  email: string;
+  token: string;
+  nome: string;
+}
+
+const transporter: Transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  secure: false,
+  auth: {
+    user: process.env.MAILER_USER as string, 
+    pass: process.env.MAILER_PASSWORD as string,
+  },
+});
+
+export const verifyUserEmail = async ({
+  email,
+  token,
+  nome,
+}: VerifyUserEmailParams): Promise<void> => {
+  try {
+    await transporter.sendMail({
+      from: process.env.MAILER_USER,
+      to: email,
+      subject: "Recuperação de senha para sua conta no Time2Play",
+      text: `Olá, ${nome}! Você solicitou a recuperação da sua senha para a sua conta no Time2Play. Caso não tenha solicitado, apenas ignore esse e-mail. Seu token de recuperação de senha é ${token}`,
+    });
+    console.log("Email enviado com sucesso");
+  } catch (err) {
+    console.error("Erro ao enviar email:", err);
+  }
+};
